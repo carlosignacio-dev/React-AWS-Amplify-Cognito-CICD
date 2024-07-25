ec3ba0b8e4ef133ad7b465d458b0b83f24481762
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2024-07-25 15:37:53 -0400

    Subiendo proyecto a github

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myquizappaaaaf388": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
